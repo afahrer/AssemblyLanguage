f618fbcac45af31c13596ef090db9a7ea9981c30
afahrer <afahrer2112@gmail.com>
2019-10-14 19:31:56 -0400

    progress on A1Q2

--- A1Q2.asm
+++ A1Q2.asm
@@ -5,30 +5,40 @@ INCLUDE Irvine32.inc
 .data
   prompt BYTE "Please enter a signed integer: ",0
   NUM = 3
-  arrayW WORD NUM DUP(0)
-  arrayD DWORD NUM DUP(0)
+  arrayW SWORD NUM DUP(0)
+  arrayD SDWORD NUM DUP(0)
 .code	
 main PROC
   ; get signed values from user
   mov ecx, NUM
   mov edx, OFFSET prompt
-  mov si, TYPE arrayW
+  mov esi, TYPE arrayW
   mov ebx, OFFSET arrayW
   l1:
     call WriteString
 	call ReadInt
 	call CrLf
-	mov bx, ax
-	add bx, si
+	mov [ebx], eax
+	add ebx, esi
   loop l1
+  mov ecx, NUM
+  mov edx, OFFSET arrayD
+  l2:
+    mov eax, [ebx] 
+    mov [edx], eax
+	add edx, esi
+	add edx, esi
+	add ebx, esi
+  loop l2
   ; print loop
   mov ecx, NUM
   mov ebx, OFFSET arrayW
   l3:
-    mov ax, bx
-	call WriteBin
+    movsx eax, DWORD PTR[edx]
+	call WriteInt
 	call CrLf
-	add bx, si
+	add edx, esi
+	add edx, esi
   loop l3
   exit
 main ENDP
