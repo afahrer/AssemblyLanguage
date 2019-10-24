de8c30d9d3587deb828d7d50f5c174cfe895241d
afahrer <afahrer2112@gmail.com>
2019-10-24 00:07:50 -0400

    added comments to A4QB

--- A4QB.asm
+++ A4QB.asm
@@ -10,31 +10,40 @@ INCLUDE Irvine32.inc
 main PROC
   mov ecx, 5
   l1:
-    mov edx, OFFSET lowerPrompt
+    ; get the lower value from user
+	mov edx, OFFSET lowerPrompt
 	call WriteString
 	call ReadInt
 	mov ebx, eax
+	; get the upper value from user
 	mov edx, OFFSET upperPrompt
 	call WriteString
 	call ReadInt
+	; store the upper value
 	mov upper, eax
+	; Store value of ecx and set ecx to 50
 	push ecx
 	mov ecx, 50
     l2:
+	  ; get a random number and print it
       call BetterRandomRange
 	  call WriteInt
 	  call CrLf
+	  ; reset eax so that a new number can be generated
 	  mov eax, upper
     loop l2
+	; restore original ecx value
 	pop ecx
   loop l1
   exit
 main ENDP
 
 BetterRandomRange PROC
+  ; subtract lower from upper and add 1 making randomRange inclusive 
   sub eax, ebx
   add eax, 1
   call RandomRange
+  ; add lower back in so that the random number cannot be too low
   add eax, ebx
   ret
 BetterRandomRange ENDP
