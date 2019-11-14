915cd51530bf078ea701501730796d86106d5133
afahrer <afahrer2112@gmail.com>
2019-11-13 20:19:13 -0500

    added comments, needs headers

--- Assignment6/A6QA.asm
+++ Assignment6/A6QA.asm
@@ -1,14 +1,14 @@
 TITLE Assignment 6 Question A (A6QA.asm)
 COMMENT !
-Description: Gets a number from the user and determines 
-             if the number is a power of 2
+Description: Creates an array of random numbers that can be
+			 multiplied, divided or printed
 Name: Adam Fahrer
-Date: November 5, 2019
+Date: November 13, 2019
 ! COMMENT
 INCLUDE Irvine32.inc
 
 .data
-  newLine = 0ah ; constant for acts like \n
+  newLine = 0ah											; constant for newLine acts like \n
   menu BYTE "1 - Populate the array with random numbers", newLine,
   "2 - Mulitply the array with a user provided multiplier", newLine,
   "3 - Divide the array with a user provided divisor", newLine,
@@ -18,79 +18,80 @@ INCLUDE Irvine32.inc
   getMult BYTE "Enter the multiplier value: ",0
   getDiv BYTE "Enter the divisor value: ",0
   commaSpace BYTE ", ",0
-  array SWORD 10 DUP(-100)
+  array SWORD 10 DUP(-100)								; SWORD array starting value -100
 .code	
 
-print PROTO,
+print PROTO,											; prototype for print procedure 
   len:DWORD,
   ref:DWORD
 
 main PROC
-  call Randomize
+  call Randomize										; generate new seed
 start:
-  mov edx, OFFSET menu
-  call WriteString
-  call ReadDec
+  mov edx, OFFSET menu									; print the menu text
+  call WriteString										
+  call ReadDec											; get the option from user
   call CrLf
-  cmp eax, 0
+  cmp eax, 0											; if eax = 0 quit
   je quit
 
 one: 
-  cmp eax, 1
+  cmp eax, 1											; eax != 1, jump to 2
   jne two 
-  push LENGTHOF array
+  push LENGTHOF array									; push lengthof array and offset array then call populate
   push OFFSET array
   call populate
   jmp start
 
 two:
-  cmp eax, 2
+  cmp eax, 2											; eax != 2, jump to 3
   jne three
-  mov edx, OFFSET getMult
+  mov edx, OFFSET getMult								; get multiplier from user
   call WriteString
   call ReadInt
-  push OFFSET array
+  push OFFSET array										; push array offset and multiplier to the stack then call multiply
   push eax
   call multiply
   jmp start
 
 three:
-  cmp eax, 3
+  cmp eax, 3											; eax != 3, jump to 4
   jne four
-  mov edx, OFFSET getDiv
+  mov edx, OFFSET getDiv								; get divisor from user
   call WriteString
   call ReadInt
-  push OFFSET array
+  push OFFSET array										; push array offset and divisor to the stack then call divide
   push eax
   call divide
   jmp start
  
 four:
-  invoke print, LENGTHOF array, OFFSET array
-  jmp start
+  invoke print, LENGTHOF array, OFFSET array			; invoke print method
+  jmp start			
+  
 quit:
   exit
 main ENDP
 
 populate PROC
 
-  ENTER 4,0				; create local variable type DWORD
+  ENTER 4,0												; create local variable type DWORD
   pushad
 
-  mov ecx, [EBP + 12]	; get lengthof array from stack
-  mov eax, -1500		; -1500 is the lower range
-  xchg eax, [EBP-4]		; store the lower range in the local variable
+  mov ecx, [EBP + 12]									; get lengthof array from stack
+  mov eax, -1500										; -1500 is the lower range
+  xchg eax, [EBP-4]										; store the lower range in the local variable
   mov ebx, [EBP+8]
 
 l1:
-  mov eax, 4001			; get random num from 0-4000 
+  mov eax, 4001											; get random num from 0-4000 
   call RandomRange
-  add eax, [EBP - 4]	; add -1500 to put number in range -1500 - +2500
+  add eax, [EBP - 4]									; add -1500 to put number in range -1500 - +2500
   mov [ebx], eax
   add ebx, TYPE WORD
   loop l1
 
-  mov edx, OFFSET popSuccess
+  mov edx, OFFSET popSuccess							; display a message when complete
   call WriteString
   call CrLf
   call CrLf
@@ -102,15 +103,17 @@ populate ENDP
 
 multiply PROC, multiple:WORD, ref:DWORD
   pushad
-  mov ecx, LENGTHOF array
+  mov ecx, LENGTHOF array		
   mov ebx, ref
+
 l1:
-  mov ax, multiple
-  mov dx, [ebx]
-  imul dx						; multiply dx with ax
-  mov [ebx], ax					; get the word value
+  mov ax, multiple										; ax is multiple
+  mov dx, [ebx]											; dx is the value from array
+  imul dx												; multiply dx with ax
+  mov [ebx], ax											; store the result in the array
   add ebx, TYPE WORD
   loop l1
+
   popad
   ret
 multiply ENDP
@@ -119,38 +122,43 @@ divide PROC, divisor:WORD, ref:DWORD
   pushad
   mov ecx, LENGTHOF array
   mov esi, ref
+
 l1:
-  mov bx, divisor
-  mov ax, [esi]
-  cwd
-  idiv bx
-  mov [esi], ax
+  mov bx, divisor										; bx is divisor
+  mov ax, [esi]											; ax is value from array
+  cwd													; convert to dword before dividing
+  idiv bx													
+  mov [esi], ax											; store the result in the array
   add esi, TYPE WORD
   loop l1
+
   popad
   ret
 divide ENDP
 
 print PROC, len:DWORD, ref:DWORD
   pushad
-  mov al, '['
+  mov al, '['											; print '['
   call WriteChar
-  mov ecx, len
-  dec ecx
+  mov ecx, len											; move array length to ecx
+  dec ecx																
   mov edx, OFFSET commaSpace
-  mov ebx, ref
+  mov ebx, ref											; ebx = OFFSET array
+
 l1:
-  movsx eax, WORD PTR [ebx]
+  movsx eax, WORD PTR [ebx]								; print array value
   call WriteInt
-  call WriteString
+  call WriteString										; print commaSpace
   add ebx, TYPE WORD
   loop l1
-  movsx eax, WORD PTR [ebx]
+
+  movsx eax, WORD PTR [ebx]								; print last value with no commaSpace
   call WriteInt
-  mov al, ']'
-  call WriteChar
+  mov al, ']'											
+  call WriteChar										; print '['
   call CrLf
   call CrLf
+
   popad
   ret
 print ENDP
